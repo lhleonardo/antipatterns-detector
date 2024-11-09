e136d54347dbd046dce68db503cd682dad6e5904
Leonardo Braz <lhleonardo05@gmail.com>
2024-11-08 22:49:51 -0300

    feat: first commit

---
+++ src/main/java/com/lhleonardo/antipatterns/detector/AntipatternsDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.lhleonardo.antipatterns.detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntipatternsDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntipatternsDetectorApplication.class, args);
+	}
+
+}
